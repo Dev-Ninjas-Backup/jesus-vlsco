feba6600dea4e1d9362ec2310cdbef8175df894e
sajib-sv <sajib.softvence@gmail.com>
2025-07-18 23:27:42 +0600

    feat: admin shift module added

---
+++ src/main/admin/shift/shift.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ShiftController } from './controller/shift.controller';
+import { ShiftService } from './services/shift.service';
+
+@Module({
+  controllers: [ShiftController],
+  providers: [ShiftService]
+})
+export class ShiftModule {}
--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './admin/user/user.module';
+import { ShiftModule } from './admin/shift/shift.module';
 import { SurveyModule } from './admin/survey/survey.module';
+import { UserModule } from './admin/user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UserModule, SurveyModule],
+  imports: [AuthModule, UserModule, SurveyModule, ShiftModule],
 })
 export class MainModule {}
