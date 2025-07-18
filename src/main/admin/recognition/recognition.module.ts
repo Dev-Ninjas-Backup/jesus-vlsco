c731ba944a3c1b8820cc491e13206fa846e1540a
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-07-19 03:14:52 +0600

    add: recognition

--- src/main/admin/recognition/recognition.module.ts
+++ src/main/admin/recognition/recognition.module.ts
@@ -4,9 +4,11 @@ import { AddBadgeController } from './controller/add-badge.controller';
 import { CloudinaryService } from '@project/lib/cloudinary/cloudinary.service';
 import { GetAllBadgeController } from './controller/get-all-badge.controller';
 import { GetAllBadgeService } from './services/get-all-badge.service';
+import { AddRecognitionService } from './services/add-recognition.service';
+import { AddRecognitionController } from './controller/add-recognition.controller';
 
 @Module({
-  providers: [AddBadgeService,CloudinaryService, GetAllBadgeService],
-  controllers: [AddBadgeController, GetAllBadgeController]
+  providers: [AddBadgeService,CloudinaryService, GetAllBadgeService, AddRecognitionService],
+  controllers: [AddBadgeController, GetAllBadgeController, AddRecognitionController]
 })
 export class RecognitionModule {}
