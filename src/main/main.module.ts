2693223e01677ebf6e854e68503d6a91d31efce7
sajib-sv <sajib.softvence@gmail.com>
2025-07-19 00:55:29 +0600

    feat: project module for admin created

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -3,8 +3,9 @@ import { ShiftModule } from './admin/shift/shift.module';
 import { SurveyModule } from './admin/survey/survey.module';
 import { UserModule } from './admin/user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ProjectModule } from './admin/project/project.module';
 
 @Module({
-  imports: [AuthModule, UserModule, SurveyModule, ShiftModule],
+  imports: [AuthModule, UserModule, SurveyModule, ShiftModule, ProjectModule],
 })
 export class MainModule {}
