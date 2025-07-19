18b16a1cfe994b6955ecc05924b86b536bd4f7c1
sajib-sv <sajib.softvence@gmail.com>
2025-07-20 04:53:12 +0600

    api :update profile endpoint completed

--- src/main/admin/user/user.module.ts
+++ src/main/admin/user/user.module.ts
@@ -4,17 +4,20 @@ import { AddUserController } from './controller/add-user.controller';
 import { EducationController } from './controller/education.controller';
 import { ExperienceController } from './controller/experience.controller';
 import { GetUserController } from './controller/get-user.controller';
+import { PayrollController } from './controller/payroll.controller';
+import { UpdateUserController } from './controller/update-user.controller';
 import { AddPayrollService } from './services/add-payroll.service';
 import { AddUserService } from './services/add-user.service';
 import { EducationService } from './services/education.service';
 import { ExperienceService } from './services/experience.service';
 import { GetUserService } from './services/get-user.service';
-import { PayrollController } from './controller/payroll.controller';
 import { PayrollService } from './services/payroll.service';
+import { UpdateUserService } from './services/update-user.service';
 
 @Module({
   controllers: [
     AddUserController,
+    UpdateUserController,
     GetUserController,
     EducationController,
     ExperienceController,
@@ -22,6 +25,7 @@ import { PayrollService } from './services/payroll.service';
   ],
   providers: [
     AddUserService,
+    UpdateUserService,
     EducationService,
     ExperienceService,
     AddPayrollService,
@@ -30,4 +34,4 @@ import { PayrollService } from './services/payroll.service';
     PayrollService,
   ],
 })
-export class UserModule {}
+export class UserModule { }
