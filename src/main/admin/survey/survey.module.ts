269c000385f850b48207bf644586feba324c3c6d
sajib-sv <sajib.softvence@gmail.com>
2025-07-21 22:44:16 +0600

    feat: added survey service and controller file

--- src/main/admin/survey/survey.module.ts
+++ src/main/admin/survey/survey.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { SurveyController } from './controller/survey.controller';
+import { SurveyService } from './services/survey.service';
 
-@Module({})
+@Module({
+  controllers: [SurveyController],
+  providers: [SurveyService]
+})
 export class SurveyModule {}
--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AnnouncementModule } from './admin/announcement/announcement.module';
 import { ProjectModule } from './admin/project/project.module';
 import { RecognitionModule } from './admin/recognition/recognition.module';
+import { SettingsModule } from './admin/settings/settings.module';
 import { ShiftModule } from './admin/shift/shift.module';
 import { SurveyModule } from './admin/survey/survey.module';
 import { TaskModule } from './admin/task/task.module';
+import { TeamModule } from './admin/team/team.module';
 import { UserModule } from './admin/user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { TeamModule } from './admin/team/team.module';
-import { SettingsModule } from './admin/settings/settings.module';
-import { AnnouncementModule } from './admin/announcement/announcement.module';
 
 @Module({
   imports: [
@@ -19,11 +19,11 @@ import { AnnouncementModule } from './admin/announcement/announcement.module';
     TeamModule,
     ProjectModule,
     TaskModule,
+    SurveyModule,
     AnnouncementModule,
     SettingsModule,
-    SurveyModule,
   ],
   controllers: [],
   providers: [],
 })
-export class MainModule {}
+export class MainModule { }
