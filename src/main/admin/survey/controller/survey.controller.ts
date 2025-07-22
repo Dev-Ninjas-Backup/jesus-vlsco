9c554f3fb138f8e57917f718a2f0d04a6b09917e
sajib-sv <sajib.softvence@gmail.com>
2025-07-23 03:00:54 +0600

    feat: survey module ongoing

--- src/main/admin/survey/controller/survey.controller.ts
+++ src/main/admin/survey/controller/survey.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { GetUser, ValidateAdmin } from '@project/common/jwt/jwt.decorator';
+import { SurveyService } from '../services/survey.service';
 
-@Controller('survey')
-export class SurveyController {}
+@ApiTags('Admin -- Survey')
+@Controller('admin/survey')
+@ValidateAdmin()
+@ApiBearerAuth()
+export class SurveyController {
+  constructor(private readonly surveyService: SurveyService) { }
+
+  @Post()
+  async createSurvey(@GetUser('userId') userId: string, @Body() dto: any) {
+    return this.surveyService.createSurvey(userId, dto);
+   }
+
+  @Post('from-template')
+  async createSurveyFromTemplate() { }
+
+  @Get()
+  async getAllSurveys() { }
+
+  @Get(':id')
+  async getSurvey() { }
+
+  @Patch(':id')
+  async updateSurvey() { }
+
+  @Delete(':id/delete')
+  async deleteSurvey() { }
+}
--- src/main/admin/survey/services/survey.service.ts
+++ src/main/admin/survey/services/survey.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { successResponse, TResponse } from '@project/common/utils/response.util';
+import { PrismaService } from '@project/lib/prisma/prisma.service';
+import { UtilsService } from '@project/lib/utils/utils.service';
 
 @Injectable()
-export class SurveyService {}
+export class SurveyService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly utils: UtilsService,
+  ) { }
+
+  async createSurvey(userId: string, dto: any): Promise<TResponse<any>> {
+    const survey = await this.prisma.survey.create({
+      data: {
+        ...dto,
+        createdBy: userId
+      }
+    })
+
+    return successResponse(survey, 'Survey created successfully');
+  }
+
+  async createSurveyFromTemplate(userId: string, dto: any) { }
+
+  async getAllSurveys() { }
+
+  async getSurvey(id: string) { }
+
+  async deleteSurvey(id: string) { }
+
+  async updateSurvey(id: string, dto: any) { }
+}
