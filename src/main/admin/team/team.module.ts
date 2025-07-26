5999c1617cf8f3c3cb7136a006eaa22329bcd40a
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-07-27 02:25:45 +0600

    add: create team

--- src/main/admin/team/team.module.ts
+++ src/main/admin/team/team.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TeamController } from './controller/team.controller';
 import { TeamService } from './services/team.service';
 import { GetAllTeamsService } from './services/get-all-team.service';
+import { CloudinaryService } from '@project/lib/cloudinary/cloudinary.service';
 
 @Module({
   controllers: [TeamController],
-  providers: [TeamService, GetAllTeamsService],
+  providers: [TeamService, GetAllTeamsService,CloudinaryService],
 })
 export class TeamModule {}
