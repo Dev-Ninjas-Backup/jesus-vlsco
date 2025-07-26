e5e68a88466c7e5fee9fc29c2bd6d6ca79c27846
sajib-sv <sajib.softvence@gmail.com>
2025-07-27 02:34:07 +0600

    frat: get projects for user endpomort adde

--- src/main/user/project/project.controller.ts
+++ src/main/user/project/project.controller.ts
@@ -1,15 +1,22 @@
 import {
-  Body,
   Controller,
-  Delete,
   Get,
-  Param,
-  Patch,
-  Post,
+  Query
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { PaginationDto } from '@project/common/dto/pagination.dto';
+import { ValidateAuth } from '@project/common/jwt/jwt.decorator';
 import { ProjectService } from './services/project.service';
 
-@Controller('project')
+@ApiTags('E -- Project')
+@Controller('employee/project')
+@ValidateAuth()
+@ApiBearerAuth()
 export class ProjectController {
-  constructor(private readonly projectService: ProjectService) {}
+  constructor(private readonly projectService: ProjectService) { }
+
+  @Get('all')
+  getAllProject(@Query() dto: PaginationDto) {
+    return this.projectService.getAllProjectWithItsTasks(dto);
+  }
 }
--- src/main/user/project/services/project.service.ts
+++ src/main/user/project/services/project.service.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PaginationDto } from '@project/common/dto/pagination.dto';
+import { HandleError } from '@project/common/error/handle-error.decorator';
+import { successPaginatedResponse, TPaginatedResponse } from '@project/common/utils/response.util';
 import { PrismaService } from '@project/lib/prisma/prisma.service';
 
 @Injectable()
 export class ProjectService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
+
+  @HandleError('Failed to get all project with its tasks')
+  async getAllProjectWithItsTasks(dto: PaginationDto): Promise<TPaginatedResponse<any>> {
+    const page = Math.max(Number(dto.page) || 1, 1);
+    const limit = Math.min(Number(dto.limit) || 10, 100);
+
+    const project = await
+      this.prisma.project.findMany({
+        include: {
+          tasks: {
+            include: {
+              tasksUsers: {
+                include: {
+                  user: true,
+                },
+              }
+            },
+          },
+        },
+        skip: (page - 1) * limit,
+        take: limit
+      })
+      console.log(project);
+
+
+    return successPaginatedResponse(project, { page: 1, limit: 10, total: 1 });
+  }
 }
