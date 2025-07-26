eac86cb51978715d5fd0f2f574828faaf899afed
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-07-26 21:59:52 +0600

    add: team chat added

--- src/main/admin/team-chat/controller/create-team-chat.controller.ts
+++ src/main/admin/team-chat/controller/create-team-chat.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { CreateTeamChatService } from '../services/create-team-chat.service';
+
 
 @Controller('create-team-chat')
-export class CreateTeamChatController {}
+export class CreateTeamChatController {
+    constructor(private readonly createTeamChat:CreateTeamChatService){}
+    async createTeamChatGroup(){
+
+    }
+}
