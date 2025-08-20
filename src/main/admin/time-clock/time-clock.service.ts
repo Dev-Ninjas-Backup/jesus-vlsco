5a2b8cb4390e2294433c2d27e50668feb660373a
shahadathhs <sajib.softvence@gmail.com>
2025-08-20 10:52:11 +0600

    feat: added get single announcment

--- src/main/admin/time-clock/time-clock.service.ts
+++ src/main/admin/time-clock/time-clock.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@project/lib/prisma/prisma.service';
 
 @Injectable()
-export class TimeClockService {}
+export class TimeClockService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async responseToShiftRequest() {}
+
+  async responseToPayrollEntryRequest() {}
+
+  async getAllPayrolls() {}
+
+  async getAllAApprovedShiftsByByAllUsers() {}
+
+  async getAllPendingShiftsByAllUsers() {}
+
+  // * get time clock of a user
+  async getAllShiftsOfAUser() {}
+}
--- src/main/user/user-time-click/user-time-click.service.ts
+++ src/main/user/user-time-click/user-time-click.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@project/lib/prisma/prisma.service';
 
 @Injectable()
-export class UserTimeClickService {}
+export class UserTimeClickService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async requestAShift() {}
+
+  // * this is time clock
+  async getAllShifts() {}
+
+  // * submit time clock (as payroll entry)
+  async submitTimeClock() {}
+
+  // * get all payrolls
+  async getAllPayrolls() {}
+}
